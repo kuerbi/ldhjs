fc243a64d8de51530af3493729a1bd5cafbffe2f
Michael Golenia <kuerbi@gmail.com>
2015-06-09 17:28:27 +0200

    some improvements and refactoring

--- ldh.js
+++ ldh.js
@@ -16,41 +16,33 @@ var saveOnLoad = window.onload;
 window.onload = function() {
   if(!prototype) return false;
 
-  var htmlElemente = {
+  var htmlBlockElements = {
     "div" :  document.getElementsByTagName('div'),
     "header" : document.getElementsByTagName('header'),
     "footer" : document.getElementsByTagName('footer'),
     "nav" : document.getElementsByTagName('nav'),
+    "table" : document.getElementsByTagName('table')
   };
-
-  var indexColor = 0;
-
-  function getNextColor() {
-    var color = colorList[indexColor];
-    indexColor = (indexColor + 1) % (colorList.length);
-
-    return color;
-  }
 
   function setElementStyles(elements) 
   {
     for(var element in elements) {
       var e = elements[element];
 
+      // Avoid Uncaught TypeError: Cannot set property 'backgroundColor' of undefined
       if (typeof e !== 'object') break;
 
-      e.style.backgroundColor = getNextColor();
+      e.style.backgroundColor = e.style.backgroundColor || colorList[element%colorList.length]
 
       if(e.id != '') e.innerHTML += ' id="'+e.id+'"';
-
       if(e.className != '') e.innerHTML += ' class="'+e.className+'"';
       if(e.innerHTML == '') e.innerHTML = '&nbsp;'
     }
   }
 
-  for(var htmlElement in htmlElemente)
+  for(var htmlBlockElement in htmlBlockElements)
   {
-    setElementStyles(htmlElemente[htmlElement]);
+    setElementStyles(htmlBlockElements[htmlBlockElement]);
   }
   
   window.onload = saveOnLoad;
